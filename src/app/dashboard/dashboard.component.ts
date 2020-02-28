8f32a5cfe1078bf9872b61f3a17e7b82b7560923
Chiadmi Saad <saad.chiadmi@gfi.fr>
2020-02-28 21:20:45 +0100

    final commit Y

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -16,15 +16,17 @@ export class DashboardComponent implements OnInit {
   data : Data[];
   filteredData : Data[];
   dechets : SelectItem[]= []; selectedDechets: string[] = [];
-  type_collecte : string[];
+  type_collecte : string[]; selectedType: string[] = [];
   years : SelectItem[]= []; selectedYear : string='';
   yearsLoop : SelectItem[]= [];
+  mounthLoop : SelectItem[]= [];
   mounths : SelectItem[] = []; selectedMounth : string='';
   sites : SelectItem[]= []; selectedSites : string='';
   raisons : SelectItem[]= []; selectedRaison : string='';
   busy: Subscription;
   dechetClo = [{ field: 'dechets', header: 'Chiffres en Kg' }];
   ngOnInit() {
+    // this.excelDatatServices.getData();
     this.setData();
     console.log(this.data);
   }
@@ -47,6 +49,10 @@ export class DashboardComponent implements OnInit {
       var workSheet = workbook.Sheets[first_sheet];
       this.data = XLSX.utils.sheet_to_json(workSheet,{raw : true})
       this.filteredData = this.data;
+      this.raisons.push({label: 'Toutes les raisons', value: ''})
+      this.sites.push({label: 'Tous les sites', value: ''})
+      this.yearsLoop.push({label: 'Toutes les années', value: ''})
+      this.mounthLoop.push({label: 'Tous les mois', value: ''})
       this.data.map(d => d.Dechet).filter((value, index, self) => self.indexOf(value) === index).sort().forEach(s => this.dechets.push({label: '' + s, value: s}));
       this.type_collecte = this.data.map(d => d.Type_collecte).filter((value, index, self) => self.indexOf(value) === index).sort();
       this.data.map(d => d.Annee).filter((value, index, self) => self.indexOf(value) === index).forEach(y => this.years.push({label: '' + y, value: y}));
@@ -55,8 +61,9 @@ export class DashboardComponent implements OnInit {
       }
       this.data.map(d => d.Site).filter((value, index, self) => self.indexOf(value) === index).forEach(s => this.sites.push({label: '' + s, value: s}));
       this.data.map(d => d.RS_client).filter((value, index, self) => self.indexOf(value) === index).forEach(rs => this.raisons.push({label: '' + rs, value: rs}));
-      this.yearsLoop=this.years;
-      console.log(this.sites);
+      this.years.forEach(y => this.yearsLoop.push(y));
+      this.mounths.forEach(m => this.mounthLoop.push(m));
+      this.selectedDechets= this.dechets.map(d => d.value);
     }
     rec.send();
     
@@ -64,9 +71,12 @@ export class DashboardComponent implements OnInit {
 
   filter(){
     this.filteredData=this.data;
-    if(this.selectedYear) {
+    if(this.selectedYear!='') {
       this.years=[{label: '' + this.selectedYear, value: this.selectedYear}];
       this.filteredData = this.filteredData.filter(d=>d.Annee==this.selectedYear);
+    }else{
+      this.years=[];
+      this.yearsLoop.filter(yl => yl.value!="").forEach(yl => this.years.push(yl));
     }
     if(this.selectedRaison!='') {
       this.filteredData = this.filteredData.filter(d => d.RS_client==this.selectedRaison);
@@ -80,8 +90,17 @@ export class DashboardComponent implements OnInit {
     if(this.selectedDechets.length>0) {
       this.filteredData = this.filteredData.filter(d => this.selectedDechets.includes(d.Dechet));
     }
+    if(this.selectedType.length>0) {
+      this.filteredData = this.filteredData.filter(d => this.selectedType.includes(d.Type_collecte));
+    }
     
     console.log(this.filteredData)
+  }
+
+  changeCheckbox(checked: boolean, type : string){
+    if(checked) this.selectedType.push(type);
+    else this.selectedType = this.selectedType.filter(t=> t!=type);
+    console.log(this.selectedType)
   }
 
   someOfMounth(dechet : string, mounth : string, year : string): string{
